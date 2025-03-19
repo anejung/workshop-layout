905656acc3fd1afeb8cfb121faa1b97c8e6c475a
Your New Username <your.new.email@example.com>
2025-03-19 16:10:02 +0700

    build menu bar

---
+++ src/app/component/MeunBar.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function MenuBar() {
+    return (
+        <nav className="fixed flex bottom-0 left-0 right-0 bg-white 
+        p-[16px] px-[100px] justify-between/>
+    )
+    
+}
